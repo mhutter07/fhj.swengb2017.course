fb3b97738ab1ea44e5635c4c432523452bf1016d
mhutter07 <martin.hutter@edu.fh-joanneum.at>
2017-11-24 09:07:06 +0100

    Merge remote-tracking branch 'upstream/master'
    # Conflicts:
    #	common/src/main/resources/at/fhj/swengb/common/github.csv

--- labs/src/test/scala/at/fhj/swengb/apps/battleship/BattleShipSpec.scala
+++ labs/src/test/scala/at/fhj/swengb/apps/battleship/BattleShipSpec.scala
@@ -5,8 +5,8 @@ import org.scalatest.WordSpecLike
 class BattleShipSpec extends WordSpecLike {
 
   val bp = BattlePos(0, 0)
-  val validxPos = (0 until 5).map(x => BattlePos(x, 0)).toSet
-  val validyPos = (0 until 5).map(y => BattlePos(0, y)).toSet
+  val validxPos = (0 until 4).map(x => BattlePos(x, 0)).toSet
+  val validyPos = (0 until 4).map(y => BattlePos(0, y)).toSet
   val illegalPos = Set(BattlePos(3, 1)) ++ validxPos.tail
 
   // TODO WP5.1 make all tests green
@@ -16,7 +16,7 @@ class BattleShipSpec extends WordSpecLike {
     "2 pos is illegal" in intercept[IllegalArgumentException](BattleShip("",Set(bp, bp)))
     "3 pos is illegal" in intercept[IllegalArgumentException](BattleShip("",Set(bp, bp, bp)))
     "4 pos is illegal" in intercept[IllegalArgumentException](BattleShip("",Set(bp, bp, bp, bp)))
-    "6 pos is illegal" in intercept[IllegalArgumentException](BattleShip("",Set(bp, bp, bp, bp)))
+    "6 pos is illegal" in intercept[IllegalArgumentException](BattleShip("",Set(bp, bp, bp, bp, bp)))
     "pos has to be horizonal" in intercept[IllegalArgumentException](BattleShip("",validxPos))
     "pos has to be vertical" in intercept[IllegalArgumentException](BattleShip("",validyPos))
     "pos is connected" in intercept[IllegalArgumentException](BattleShip("",illegalPos))
